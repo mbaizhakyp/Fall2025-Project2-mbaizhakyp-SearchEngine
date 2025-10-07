a4cdc15bac9d21d37bf1bc5465e3d07f1a307829
mbaizhakyp <mbaizhakyp@crimson.ua.edu>
2025-10-07 09:43:11 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Fall2025_Project2_mbaizhakyp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
